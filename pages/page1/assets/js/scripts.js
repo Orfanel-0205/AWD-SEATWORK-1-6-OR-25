a0eff3a9a7d3e8d8e5fe3fd8aabc746babde071c
Orfanel-0205 <202211750@fit.edu.ph>
2025-01-29 01:02:47 -0800

    feat: Add GWA calculator functionality with styled UI

---
+++ pages/page1/assets/js/scripts.js
@@ -0,0 +1,22 @@
+document.getElementById('gradeForm').addEventListener('submit' , function(event) {
+    event.preventDefault();
+    
+
+    const name = document.getElementById('name').value;
+    const grades = [
+       parseFloat (document.getElementById('grade1').value),
+       parseFloat (document.getElementById('grade2').value),
+       parseFloat (document.getElementById('grade3').value),
+       parseFloat (document.getElementById('grade4').value),
+       parseFloat (document.getElementById('grade5').value),
+    ];
+    
+    const gwa = grades.reduce((a, b) => a + b) / grades.length;
+    const resultElement = document.getElementById('result');
+    resultElement.classList.remove('hidden');
+    resultElement.innerHTML = `
+        <p>Name: ${name}</p>
+        <p>GWA: ${gwa}</p>
+    `;
+
+});
